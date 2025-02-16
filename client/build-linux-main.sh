8d59f18ad17ef80dd45c73644aa5102538cbabc8
Dilan Gilluly <gillulyd@gmail.com>
2025-02-16 14:12:08 -0500

    more complete build script

--- client/build-linux-main.sh
+++ client/build-linux-main.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-pyinstaller --noconsole --onefile --hidden-import='PIL._tkinter_finder' --icon='icon.ico' main.py
+pyinstaller --noconsole --contents-directory='tam' --hidden-import='PIL._tkinter_finder' --icon='icon.ico' main.py
+pyinstaller --contents-directory='tam' --icon='icon.ico' backup_restore.py
+
+cp icon.png dist/backup_restore/backup_restore dist/main/
+mv dist/main/backup_restore dist/main/tam-backup-restore
+mv dist/main/main dist/main/tam-client
